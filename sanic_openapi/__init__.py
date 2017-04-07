777c7de6b48929e92d8cc57b4ae57e7ffbf7a702
Michael Hill <mhill@riotgames.com>
2017-04-07 02:54:15 -0700

    Last one for realsies

--- sanic_openapi/__init__.py
+++ sanic_openapi/__init__.py
@@ -1,5 +1,5 @@
 from .openapi import blueprint as openapi_blueprint
 from .swagger import blueprint as swagger_blueprint
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __all__ = ['openapi_blueprint', 'swagger_blueprint']
