c28bdbcc305210ce1f15a22697e4bb5fcb1df0d0
zhoujingfeng <zjf@yihe.io>
2017-09-11 14:26:23 +0800

    fix more params traversal

--- sanic_openapi/openapi.py
+++ sanic_openapi/openapi.py
@@ -103,6 +103,7 @@ def build_spec(app, loop):
                             'in': consumer.location,
                             'name': name
                         }
+                        route_parameters.append(route_param)
                 else:
                     route_param = {
                         **spec,
@@ -110,13 +111,11 @@ def build_spec(app, loop):
                         'in': consumer.location,
                         'name': consumer.field.name if hasattr(consumer.field, 'name') else 'body'
                     }
-
+                    route_parameters.append(route_param)
                 if '$ref' in route_param:
                     route_param["schema"] = {'$ref': route_param['$ref']}
                     del route_param['$ref']
-
-                route_parameters.append(route_param)
-
+                    route_parameters.append(route_param)
             endpoint = remove_nulls({
                 'operationId': route_spec.operation or route.name,
                 'summary': route_spec.summary,
