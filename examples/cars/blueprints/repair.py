df7724fa344a8feefede5168541433e19e969b5c
Guoli Lyu <guoli-lv@hotmail.com>
2018-05-10 15:23:38 +0800

    feat(examples): add class based routing demonstration in cars example

---
+++ examples/cars/blueprints/repair.py
@@ -0,0 +1,23 @@
+from sanic.views import HTTPMethodView
+from sanic.blueprints import Blueprint
+from sanic.response import json
+from sanic_openapi import doc
+
+blueprint = Blueprint('Repair', '/repair')
+from models import Station
+from data import test_station
+
+
+class RepairStation(HTTPMethodView):
+    @doc.summary("Fetches all repair stations")
+    @doc.produces([Station])
+    def get(self, request):
+        return json([test_station])
+
+    @doc.summary("make an appointment")
+    @doc.description("submit necessary information for appointment")
+    def post(self, request):
+        return json(request.json)
+
+
+blueprint.add_route(RepairStation.as_view(), "/station", strict_slashes=True)
