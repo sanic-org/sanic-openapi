17b7ab5171ad6b708c54b08e89b55022369c702a
ferdina.kusumah@gmail.com <fh3nh4FH3NH4>
2019-04-18 15:14:32 +0700

    Request features:     1. Adding new UI to existing ui     2. Adding deprecated features
    How to use it:
    ```python
        ...
        @app.post("/api/post", strict_slashes=True)
        @doc.summary("Create new users")
        @doc.deprecated(True)
        @doc.consumes(doc.JsonBody({"user": {"name": str}}), location="body")
        ...
    ```
    
    Change Notes:
    1. File doc.py
        a. Define `deprecated` value to None at line 213
        b. Adding `deprecated` value to function route at line 244
        c. Adding checker `deprecated` value at line 252
        d. Adding new function `deprecated` at line 273
    
    2. File swagger.py
        a. Adding `deprecated` value in `endpoint` values at line 157
    
    3. Adding new ui swagger

--- sanic_openapi/doc.py
+++ sanic_openapi/doc.py
@@ -210,6 +210,7 @@ class RouteSpec(object):
     produces = None
     produces_content_type = None
     summary = None
+    deprecated = None
     description = None
     operation = None
     blueprint = None
@@ -240,7 +241,7 @@ class RouteField(object):
 route_specs = defaultdict(RouteSpec)
 
 
-def route(summary=None, description=None, consumes=None, produces=None,
+def route(summary=None, deprecated=None, description=None, consumes=None, produces=None,
           consumes_content_type=None, produces_content_type=None,
           exclude=None, response=None):
     def inner(func):
@@ -248,6 +249,8 @@ def route(summary=None, description=None, consumes=None, produces=None,
 
         if summary is not None:
             route_spec.summary = summary
+        if deprecated is not None:
+            route_spec.deprecated = deprecated
         if description is not None:
             route_spec.description = description
         if consumes is not None:
@@ -266,6 +269,12 @@ def route(summary=None, description=None, consumes=None, produces=None,
         return func
     return inner
 
+
+def deprecated(boolean):
+    def inner(func):
+        route_specs[func].deprecated = boolean
+        return func
+    return inner
 
 def exclude(boolean):
     def inner(func):
