1079e15d485c5f7f3a3286890ef533fe33920b6e
jacob <chenjr0719@gmail.com>
2019-06-08 19:14:24 +0800

    Build: Use setup.py to control dependencies and remove requirements files

--- setup.py
+++ setup.py
@@ -4,36 +4,54 @@ Sanic
 import codecs
 import os
 import re
+
 from setuptools import setup
 
+install_requires = ["sanic>=18.12.0"]
 
-with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
-        __file__)), 'sanic_openapi', '__init__.py'), 'r', 'latin1') as fp:
+dev_requires = ["black==19.3b0", "flake8==3.7.7", "isort==4.3.19"]
+
+test_requires = [
+    "coverage==4.5.3",
+    "pytest==4.6.2",
+    "pytest-cov==2.7.1",
+    "pytest-runner==5.1",
+    "tox==3.12.1",
+]
+
+project_root = os.path.dirname(os.path.abspath(__file__))
+
+with codecs.open(
+    os.path.join(project_root, "sanic_openapi", "__init__.py"), "r", "latin1"
+) as fp:
     try:
-        version = re.findall(r"^__version__ = '([^']+)'\r?$",
-                             fp.read(), re.M)[0]
+        version = re.findall(r"^__version__ = '([^']+)'\r?$", fp.read(), re.M)[0]
     except IndexError:
-        raise RuntimeError('Unable to determine version.')
+        raise RuntimeError("Unable to determine version.")
+
+with open(os.path.join(project_root, "README.md"), "r") as f:
+    long_description = f.read()
 
 setup(
-    name='sanic-openapi',
+    name="sanic-openapi",
     version=version,
-    url='http://github.com/channelcat/sanic-openapi/',
-    license='MIT',
-    author='Channel Cat',
-    author_email='channelcat@gmail.com',
-    description='OpenAPI support for Sanic',
-    packages=['sanic_openapi'],
-    package_data={'sanic_openapi': ['ui/*']},
-    platforms='any',
-    install_requires=[
-        'sanic>=0.7.0',
-    ],
+    url="http://github.com/channelcat/sanic-openapi/",
+    license="MIT",
+    author="Sanic Community",
+    description="Easily document your Sanic API with a UI.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["sanic_openapi"],
+    package_data={"sanic_openapi": ["ui/*"]},
+    platforms="any",
+    install_requires=install_requires,
+    extras_require={"dev": dev_requires + test_requires, "test": test_requires},
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Web Environment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
