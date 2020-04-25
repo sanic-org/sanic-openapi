6c0231d765400e503da9a835873d01407aa09186
Adam Bannister <adam.p.bannister@gmail.com>
2020-04-25 16:29:06 +0200

    add tests for spec

---
+++ tests/test_spec.py
@@ -0,0 +1,40 @@
+from datetime import date
+
+
+from sanic_openapi.spec import Spec
+
+
+def test_build_definition_with_containing_class():
+    spec = Spec(config={})
+
+    class Manufacturer:
+        name: str
+        start_date: date
+
+    class Car:
+        on: bool
+        doors: int
+        color: str
+        make: Manufacturer
+
+    spec._build_definition(definition=Car)
+
+    assert len(spec.definitions) == 2
+
+
+def test_build_definition_with_list_of_class():
+    spec = Spec(config={})
+
+    class Manufacturer:
+        name: str
+        start_date: date
+
+    class Car:
+        on: bool
+        doors: int
+        color: str
+        makers: [Manufacturer]
+
+    spec._build_definition(definition=Car)
+
+    assert len(spec.definitions) == 2
