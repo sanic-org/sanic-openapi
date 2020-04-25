14006a511989a95a0c09838781863fcf000e6a75
Adam Bannister <adam.p.bannister@gmail.com>
2020-04-25 16:28:34 +0200

    fix type assignment in example

--- examples/cars/models.py
+++ examples/cars/models.py
@@ -4,8 +4,8 @@ from sanic_openapi import doc
 
 
 class Manufacturer:
-    name = str
-    start_date = date
+    name: str
+    start_date: date
 
 
 class Driver:
@@ -14,23 +14,23 @@ class Driver:
 
 
 class Car:
-    on = bool
-    doors = int
-    color = str
-    make = Manufacturer
-    driver = Driver
-    passengers = [Driver]
+    on: bool
+    doors: int
+    color: str
+    make: Manufacturer
+    driver: Driver
+    passengers: [Driver]
 
 
 class Garage:
-    spaces = int
-    cars = [Car]
+    spaces: int
+    cars: [Car]
 
 
 class Status:
-    success = bool
+    success: bool
 
 
 class Station:
-    location = doc.String("location")
-    contact = doc.Integer("phone number")
+    location: doc.String("location")
+    contact: doc.Integer("phone number")
