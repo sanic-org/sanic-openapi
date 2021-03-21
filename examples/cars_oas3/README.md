227800a5551262b58916c3109aecf7847544a94d
Andrea Stagi <stagi.andrea@gmail.com>
2021-03-21 16:40:29 +0100

    feat: add oas3 example

---
+++ examples/cars_oas3/README.md
@@ -0,0 +1,21 @@
+## Cars
+
+[Source](https://github.com/huge-success/sanic-openapi/tree/master/examples/cars)
+
+This example shows how to use Sanic-OpenAPI with Sanic's Blueprint and model all the data into Sanic-OpenAPI's fields.
+
+To run this example, please make sure you have already install `sanic` and `sanic-openapi`.
+Or, run the install command:
+
+```shell
+pip install sanic sanic-openapi
+```
+
+And, you can run this example by:
+
+```shell
+python main.py
+```
+
+Now, check <http://localhost:8000/swagger> and you should see the swagger like:
+![](./swagger.png)
--- examples/cars_oas3/main.py
+++ examples/cars_oas3/main.py
@@ -5,16 +5,17 @@ from blueprints.manufacturer import blueprint as manufacturer_blueprint
 from blueprints.repair import blueprint as repair_blueprint
 from sanic import Sanic
 
-from sanic_openapi import swagger_blueprint
+from sanic_openapi import oas3_blueprint
 
 app = Sanic("Cars API example")
 
-app.blueprint(swagger_blueprint)
 app.blueprint(car_blueprint)
 app.blueprint(driver_blueprint)
 app.blueprint(garage_blueprint)
 app.blueprint(manufacturer_blueprint)
 app.blueprint(repair_blueprint)
+
+app.blueprint(oas3_blueprint)
 
 app.config.API_VERSION = '1.0.0'
 app.config.API_TITLE = 'Car API'
@@ -22,4 +23,4 @@ app.config.API_TERMS_OF_SERVICE = 'Use with caution!'
 app.config.API_CONTACT_EMAIL = 'channelcat@gmail.com'
 app.config.API_DESCRIPTION = 'Cars API example'
 
-app.run(host="0.0.0.0", debug=True)
+app.run(host="0.0.0.0", port="8001", debug=True)
