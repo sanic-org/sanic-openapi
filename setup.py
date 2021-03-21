16cb94b9b80894aa18ba4baf04e632cc4ff20eea
artcg <arthurgoldbergfwd@gmail.com>
2021-03-21 19:06:36 +0100

    update setup.py classifiers

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open(os.path.join(project_root, "README.md"), "r") as f:
 setup(
     name="sanic-openapi",
     version=version,
-    url="http://github.com/channelcat/sanic-openapi/",
+    url="http://github.com/sanic-org/sanic-openapi/",
     license="MIT",
     author="Sanic Community",
     description="Easily document your Sanic API with a UI.",
@@ -54,11 +54,11 @@ setup(
         "doc": doc_requires,
     },
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
