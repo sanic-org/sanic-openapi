17505ed5e93ca20df8e401328245df38993db03c
Adam Hopkins <admhpkns@gmail.com>
2021-05-04 23:58:32 +0300

    squash

--- sanic_openapi/utils.py
+++ sanic_openapi/utils.py
@@ -36,7 +36,11 @@ def remove_nulls(dictionary, deep=True):
     """
     Removes all null values from a dictionary.
     """
-    return {k: remove_nulls(v, deep) if deep and type(v) is dict else v for k, v in dictionary.items() if v is not None}
+    return {
+        k: remove_nulls(v, deep) if deep and type(v) is dict else v
+        for k, v in dictionary.items()
+        if v is not None
+    }
 
 
 def remove_nulls_from_kwargs(**kwargs):
@@ -49,28 +53,13 @@ def get_blueprinted_routes(app):
             continue
 
         for route in blueprint.routes:
-            if not hasattr(route, "handler"):
-                # in sanic 21.3, "route" is now something different:
-                #   an instance of sanic_routing.route.Route
-                # can't typecheck because code needs to be compatible
-                # with older sanic versions, but "handler" does not exist
-                # on sanic>=21.3 route
-                #
-                # the good news - now class based views are already handled
-                for method_handlers in route.handlers.values():
-                    for handler in method_handlers.values():
-                        if len(handler):
-                            # handler is a list of functions
-                            # can be size zero for some reason, not sure when
-                            # that occurs ...
-                            yield (blueprint.name, handler[0])
-                continue
-
             if hasattr(route.handler, "view_class"):
                 # before sanic 21.3, route.handler could be a number of
                 # different things, so have to type check
                 for http_method in route.methods:
-                    _handler = getattr(route.handler.view_class, http_method.lower(), None)
+                    _handler = getattr(
+                        route.handler.view_class, http_method.lower(), None
+                    )
                     if _handler:
                         yield (blueprint.name, _handler)
             else:
@@ -104,13 +93,19 @@ def get_all_routes(app, skip_prefix):
                     continue
 
                 for parameter in group.params.values():
-                    uri = re.sub("<" + parameter.name + ".*?>", "{" + parameter.name + "}", uri)
+                    uri = re.sub(
+                        "<" + parameter.name + ".*?>",
+                        "{" + parameter.name + "}",
+                        uri,
+                    )
 
                 for route in group:
                     if route.name and "static" in route.name:
                         continue
 
-                    method_handlers = [(method, route.handler) for method in route.methods]
+                    method_handlers = [
+                        (method, route.handler) for method in route.methods
+                    ]
 
                     _, name = route.name.split(".", 1)
                     yield (uri, name, route.params.values(), method_handlers)
@@ -138,12 +133,19 @@ def get_all_routes(app, skip_prefix):
 
             elif hasattr(route.handler, "view_class"):
                 method_handlers = {
-                    method: getattr(route.handler.view_class, method.lower()) for method in route.methods
+                    method: getattr(route.handler.view_class, method.lower())
+                    for method in route.methods
                 }
             else:
-                method_handlers = {method: route.handler for method in route.methods}
+                method_handlers = {
+                    method: route.handler for method in route.methods
+                }
 
             for parameter in route.parameters:
-                uri = re.sub("<" + parameter.name + ".*?>", "{" + parameter.name + "}", uri)
+                uri = re.sub(
+                    "<" + parameter.name + ".*?>",
+                    "{" + parameter.name + "}",
+                    uri,
+                )
 
             yield uri, route.name, route.parameters, method_handlers.items()
